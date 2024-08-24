63d36b2e376f3f87bb3e31d69e3545e1bcab075d
V-dhumal <146967997+V-dhumal@users.noreply.github.com>
2024-08-24 20:58:55 +0530

    Update Delete.jsx

--- src/components/Delete/Delete.jsx
+++ src/components/Delete/Delete.jsx
@@ -4,7 +4,7 @@ import  del from './trash.png'
 function Delete({Tasks}) {
   return (
     <div>
-        <h2 className=''>{Tasks}</h2>
+        <h2 className=' '>{Tasks}</h2>
         <img src={del} className=' h-10 w-10'/>
     </div> 
   )  
